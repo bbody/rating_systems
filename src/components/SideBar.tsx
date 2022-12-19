9f2767fb5f8c2c74fb676f13e19c405c815c5b18
Brendon Body <bbody.project@gmail.com>
2022-12-19 18:46:31 +1100

    Make sidebar toggleable

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,10 +2,10 @@ import React, { Fragment } from "react"
 import languages from "../data/languages"
 import LanguageSideBarItem from "./LanguageSideBarItem"
 
-export default function SideBar() {
+export default function SideBar({showSidebar}: {showSidebar: boolean}) {
     return (
         <Fragment>
-            <aside id="sidebar" className="fixed hidden z-20 h-full top-0 left-0 pt-16 flex lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75" aria-label="Sidebar">
+            <aside id="sidebar" className={`fixed z-20 h-full top-0 left-0 pt-16 flex lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75` + (showSidebar ? "" : " hidden")} aria-label="Sidebar">
                 <div className="relative flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white pt-0">
                     <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
                         <div className="flex-1 px-3 bg-white divide-y space-y-1">
@@ -29,7 +29,7 @@ export default function SideBar() {
                     </div >
                 </div >
             </aside >
-            <div className="bg-gray-900 opacity-50 hidden fixed inset-0 z-10" id="sidebarBackdrop"></div>
+            <div className={`bg-gray-900 opacity-50 fixed inset-0 z-10` + (showSidebar ? "" : " hidden")} id="sidebarBackdrop"></div>
         </Fragment >
     )
 }
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,14 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import { Outlet } from "react-router-dom"
 import NavBar from "../components/NavBar"
 import SideBar from "../components/SideBar"
 
 export default function Layout() {
+    const [showSidebar, setShowSidebar] = useState(false)
+    const toggleSidebar = () => {
+        setShowSidebar(!showSidebar)
+    }
     return (
         <>
-            <NavBar />
+            <NavBar showSidebarToggle={toggleSidebar} sidebarOpen={showSidebar} />
             <div className="flex overflow-hidden bg-white pt-16">
-                <SideBar />
+                <SideBar showSidebar={showSidebar}/>
                 <div id="main-content" className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
                     <main>
                         <Outlet />
